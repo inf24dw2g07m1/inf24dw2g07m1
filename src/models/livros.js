13c1d92cde794b670e05ff24024fe2e7e16366e5
Alesandro0012 <100311143+Alesandro0012@users.noreply.github.com>
2025-05-14 19:52:49 +0100

    commit

--- src/models/livros.js
+++ src/models/livros.js
@@ -1,12 +1,12 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./database');
+const sequelize = require('../config/database');
 
 const Cliente = sequelize.define('Cliente', {
   nome: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  
+
 });
 
 module.exports = Cliente;
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 
 const { DataTypes } = require('sequelize');
-const sequelize = require('./database');
+const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
   nome: {
